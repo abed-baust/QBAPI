87c52a170436d04eab62981c229eac313018869a
Abedur <abed.baust@gmail.com>
2022-09-18 22:31:37 +0600

    [User Role , Authentication JWT token]

---
+++ QBAPI/QBAPI/AuthRoles/UserRoles.cs
@@ -0,0 +1,10 @@
+﻿namespace QBAPI.AuthRoles
+{
+    public static class UserRoles
+    {
+        public const string SuperAdmin = "SuperAdmin";
+        public const string Student = "Student";
+        public const string Teacher = "Teacher";
+        public const string UploaderCR = "UploaderAdmin";
+    }
+}
